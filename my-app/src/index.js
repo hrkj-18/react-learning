a8749d2fe6202ba34f8a85566069712ab0425d52
Harsh Jain <1997harsh.jain@gmail.com>
2022-11-30 12:44:29 +0530

    Added Sentry Crash Report

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,6 +10,13 @@ const {REACT_APP_MYENV} = process.env;
 
 Sentry.init({
   dsn: "https://0649083cd7764b049472847bc3d65689@o4504246743138304.ingest.sentry.io/4504246744121344",
+  beforeSend(event, hint) {
+    // Check if it is an exception, and if so, show the report dialog
+    if (event.exception) {
+      Sentry.showReportDialog({ eventId: event.event_id });
+    }
+    return event;
+  },
   integrations: [new BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%
